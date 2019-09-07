cecb11f611366c722efbf6f4b818cdb62ba62a68
Jiri Velek <bleksak@pop-os.localdomain>
2019-09-07 15:37:24 +0200

    small fix

--- cmake/CMakeFiles/grub.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/grub"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/grub.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
