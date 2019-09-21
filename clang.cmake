53982eb0dd9521811f23a2013abe150897fd63a5
Bleksak <bleksak@pm.me>
2019-09-21 19:39:58 +0200

    add clang support

--- clang.cmake
+++ clang.cmake
@@ -1,13 +1,13 @@
-set(CMAKE_SYSTEM Generic)
-set(CMAKE_SYSTEM_NAME Generic)
+# set(CMAKE_SYSTEM Generic)
+# set(CMAKE_SYSTEM_NAME Generic)
 
 INCLUDE(CMakeForceCompiler)
 CMAKE_FORCE_C_COMPILER(clang GNU)
 
-# set(CMAKE_C_COMPILER clang)
 set(CMAKE_C_LINKER_PREFERENCE clang)
 
-SET(CMAKE_C_SIZEOF_DATA_PTR 4)
+set(CMAKE_C_SIZEOF_DATA_PTR 4)
 
 set(CMAKE_C_FLAGS "-D _x86 -x c -std=c17 -Wall -Wextra -ffreestanding -nostdlib -nostdinc -msse2 --target=i686-pc-none-elf -march=i686")
-set(CMAKE_C_LINK_EXECUTABLE "${CMAKE_C_COMPILER} <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+set(CMAKE_C_LINK_FLAGS "-nostdlib -l libgcc.a -T${PROJECT_SOURCE_DIR}/linker.ld")
+set(CMAKE_C_LINK_EXECUTABLE "ld.lld <OBJECTS> -o <TARGET> <LINK_LIBRARIES> <CMAKE_C_LINK_FLAGS>")
--- gcc.cmake
+++ gcc.cmake
@@ -2,8 +2,6 @@ INCLUDE(CMakeForceCompiler)
 CMAKE_FORCE_C_COMPILER(i686-elf-gcc GNU)
 SET(CMAKE_C_SIZEOF_DATA_PTR 4)
 
-set(CMAKE_C_FLAGS "-D _x86 -x c -Wno-cpp -std=c17 -Wall -Wextra -ffreestanding -nostdlib -march=i686")
 set(CMAKE_C_LINK_EXECUTABLE "${CMAKE_C_COMPILER} <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
 
-
-# SET(CMAKE_C_FLAGS "-O2 -g -ffreestanding -fbuiltin -Wall -Wextra -std=gnu99 -msse" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS "-D _x86 -x c -Wno-cpp -std=c17 -Wall -Wextra -ffreestanding -nostdlib -nostdinc -march=i686 -msse2")
