9390af7262ee087b6d23876bd5268c7d4bafa1a2
Jiri Velek <bleksak@pop-os.localdomain>
2019-09-21 21:26:22 +0200

    add clang support

--- clang.cmake
+++ clang.cmake
@@ -1,13 +1,8 @@
-# set(CMAKE_SYSTEM Generic)
-# set(CMAKE_SYSTEM_NAME Generic)
-
 INCLUDE(CMakeForceCompiler)
-CMAKE_FORCE_C_COMPILER(clang GNU)
-
-set(CMAKE_C_LINKER_PREFERENCE clang)
+CMAKE_FORCE_C_COMPILER(clang-9 GNU)
 
 set(CMAKE_C_SIZEOF_DATA_PTR 4)
 
 set(CMAKE_C_FLAGS "-D _x86 -x c -std=c17 -Wall -Wextra -ffreestanding -nostdlib -nostdinc -msse2 --target=i686-pc-none-elf -march=i686")
-set(CMAKE_C_LINK_FLAGS "-nostdlib -l libgcc.a -T${PROJECT_SOURCE_DIR}/linker.ld")
-set(CMAKE_C_LINK_EXECUTABLE "ld.lld <OBJECTS> -o <TARGET> <LINK_LIBRARIES> <CMAKE_C_LINK_FLAGS>")
+set(CMAKE_C_LINK_FLAGS "-nostdlib -lgcc -T${PROJECT_SOURCE_DIR}/linker.ld --target=i686-pc-none-elf -march=i686")
+set(CMAKE_C_LINK_EXECUTABLE "clang-9 <OBJECTS> -o <TARGET> <LINK_LIBRARIES> <CMAKE_C_LINK_FLAGS>")
--- gcc.cmake
+++ gcc.cmake
@@ -2,6 +2,8 @@ INCLUDE(CMakeForceCompiler)
 CMAKE_FORCE_C_COMPILER(i686-elf-gcc GNU)
 SET(CMAKE_C_SIZEOF_DATA_PTR 4)
 
-set(CMAKE_C_LINK_EXECUTABLE "${CMAKE_C_COMPILER} <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+set(CMAKE_C_LINK_FLAGS "-nostdlib -T ${PROJECT_SOURCE_DIR}/linker.ld -lgcc")
+
+set(CMAKE_C_LINK_EXECUTABLE "${CMAKE_C_COMPILER} <OBJECTS> -o <TARGET> <LINK_LIBRARIES> <CMAKE_C_LINK_FLAGS>")
 
 set(CMAKE_C_FLAGS "-D _x86 -x c -Wno-cpp -std=c17 -Wall -Wextra -ffreestanding -nostdlib -nostdinc -march=i686 -msse2")
