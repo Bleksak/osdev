5e1e2256edfda289e6efc9683af0a3f2b83352af
Jiri Velek <bleksak@pop-os.localdomain>
2019-10-28 16:03:25 +0100

    Add Vector type

--- src/std.h
+++ src/std.h
@@ -86,3 +86,22 @@ typedef __builtin_va_list __gnuc_va_list;
 typedef __gnuc_va_list va_list;
 
 #endif
+
+#define Vector(name, T) \
+    struct name { \
+        size_t length;\
+        T* entries;\
+    }; \
+    typedef struct name name
+
+#define VectorInstance(name, length) \
+    (name) {.length = length, .entries = calloc(sizeof(name), length)}
+
+#define VectorPush(vec, value, result) \
+    result = realloc(vec.entries, (vec.length + 1) * sizeof(vec.entries[0])); \
+    if(result) { \
+        vec.entries = result; \
+        memcpy((void*) &vec.entries[vec.length], (void*) &value, sizeof(vec.entries[0])); \
+        vec.length++; \
+    } \
+    
