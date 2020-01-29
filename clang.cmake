72b0ba72b1e48b89411433e975f48411141a7575
Jiri Velek <bleksak@protonmail.com>
2020-01-29 14:42:38 +0100

    rework build system (still need i686-elf-gcc even for clang)

--- clang.cmake
+++ clang.cmake
@@ -2,10 +2,19 @@ set(CMAKE_SYSTEM Generic)
 set(CMAKE_SYSTEM_NAME Generic)
 
 INCLUDE(CMakeForceCompiler)
+
+# set(CMAKE_C_COMPILER clang)
+
 CMAKE_FORCE_C_COMPILER(clang Clang)
 
 set(CMAKE_C_SIZEOF_DATA_PTR 4)
 
-set(CMAKE_C_FLAGS "-D _x86 -Wpedantic -x c -Wall -Wextra -ffreestanding -msse2 -ansi -std=c17")
-set(CMAKE_C_LINK_FLAGS "-nostdlib -lgcc -T${PROJECT_SOURCE_DIR}/linker.ld")
-set(CMAKE_C_LINK_EXECUTABLE "i686-elf-gcc <OBJECTS> -o <TARGET> <LINK_LIBRARIES> <CMAKE_C_LINK_FLAGS>")
+# set(CMAKE_C_LINK_EXECUTABLE "i686-elf-gcc <OBJECTS> -o <TARGET> <LINK_LIBRARIES> <CMAKE_C_LINK_FLAGS>")
+
+set(CMAKE_BUILD_TYPE Release)
+
+execute_process(COMMAND i686-elf-gcc "-print-libgcc-file-name" OUTPUT_VARIABLE FILE_LIBGCC)
+
+set(CMAKE_C_FLAGS "-D _x86 -Wpedantic -x c -Wall -Wextra -ffreestanding -ansi -std=c17 --target=i686-pc-none-elf -march=i686")
+set(CMAKE_C_LINK_FLAGS "-T${PROJECT_SOURCE_DIR}/linker.ld -flto --target=i686-pc-none-elf -nostdlib ${FILE_LIBGCC}")
+set(CMAKE_C_LINK_EXECUTABLE "clang <OBJECTS> -o <TARGET> <LINK_LIBRARIES> <CMAKE_C_LINK_FLAGS>")
